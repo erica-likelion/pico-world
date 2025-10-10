5fa9e8e02d08f9f0c0194ba8c8b9bcbe5e7d968a
javadocq <han18264@naver.com>
2025-10-11 04:03:37 +0900

    feat: storybook

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,4 +1,5 @@
 import type { StorybookConfig } from "@storybook/react-native-web-vite";
+import { mergeConfig } from "vite";
 
 const config: StorybookConfig = {
 	stories: [
@@ -10,5 +11,23 @@ const config: StorybookConfig = {
 		name: "@storybook/react-native-web-vite",
 		options: {},
 	},
+
+	// ✅ 여기서 Vite 설정을 수정할 수 있음
+	viteFinal: async (config) => {
+		return mergeConfig(config, {
+			resolve: {
+				alias: {
+					// ✅ Node 버전 debug 대신 브라우저 버전 사용
+					"debug/src/node": "debug/src/browser",
+				},
+			},
+			define: {
+				// ✅ process나 global 같은 Node 전역 객체 참조 방지
+				"process.env": {},
+				global: "globalThis",
+			},
+		});
+	},
 };
+
 export default config;
