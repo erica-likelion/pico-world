bd2c0f2a0071cff8387499b28af39df89d5489ed
javadocq <han18264@naver.com>
2025-10-25 12:54:00 +0900

    feat: card-edit

--- shared/ui/EmotionCard.tsx
+++ shared/ui/EmotionCard.tsx
@@ -1,17 +1,37 @@
 import * as S from "@/shared/style/EmotionCard.styles";
 import { useId } from "react";
+import { TouchableOpacity } from "react-native";
 import { Defs, RadialGradient, Rect, Stop, Svg } from "react-native-svg";
 
 interface EmotionCardProps {
 	mainColor: string;
 	subColor: string;
 	title: string;
+	isEditor?: boolean;
+	onPress?: () => void;
 }
 
-export function EmotionCard({ mainColor, subColor, title }: EmotionCardProps) {
+export function EmotionCard({
+	mainColor,
+	subColor,
+	title,
+	isEditor = false,
+	onPress,
+}: EmotionCardProps) {
 	const gradientId = useId();
 	return (
 		<S.EmotionCardContainer $subColor={subColor}>
+			{isEditor && (
+				<S.EditBox>
+					<S.EditDateBox>
+						<S.EditDate>2025. 10. 6</S.EditDate>
+						<S.EditTime>오후 3:45</S.EditTime>
+					</S.EditDateBox>
+					<TouchableOpacity onPress={onPress} activeOpacity={0.8}>
+						<S.MenuIcon />
+					</TouchableOpacity>
+				</S.EditBox>
+			)}
 			<Svg width="313" height="190" style={{ position: "absolute" }}>
 				<Defs>
 					<RadialGradient
