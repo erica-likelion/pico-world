09586f88d1987cb8862ae4aa4b85d6edaea5cfef
gyuhochoime <cjg1999@naver.com>
2025-10-31 03:42:24 +0900

    feat: 기록 페이지 topnav 아이콘 추가 및 아이콘 클릭 시  경로 설정

---
+++ app/journal/calendar/detail.tsx
@@ -0,0 +1,13 @@
+import { TopNav } from "@/widgets/TopNav/ui";
+import { useRouter } from "expo-router";
+import { View } from "react-native";
+
+export default function JournalCalendarDetail() {
+	const router = useRouter();
+
+	return (
+		<View style={{ flex: 1 }}>
+			<TopNav title="달력" leftIcon={true} onLeftPress={() => router.back()} />
+		</View>
+	);
+}
--- app/journal/index.tsx
+++ app/journal/index.tsx
@@ -1,6 +1,7 @@
+import CalendarIcon from "@/shared/assets/icons/calendar.svg";
 import { useBottomNavStore } from "@/widgets/BottomNav/model";
 import { TopNav } from "@/widgets/TopNav/ui";
-import { useFocusEffect, useRouter } from "expo-router";
+import { useFocusEffect, useRouter, type Href } from "expo-router";
 import { useCallback } from "react";
 import { Text, TouchableOpacity } from "react-native";
 
@@ -16,7 +17,11 @@ export default function Journal() {
 
 	return (
 		<>
-			<TopNav title="기록" />
+			<TopNav
+				title="기록"
+				rightIcon={<CalendarIcon />}
+				onRightPress={() => router.push("/journal/calendar" as Href)}
+			/>
 			<TouchableOpacity onPress={() => router.push("../record")}>
 				<Text style={{ color: "white" }}>감정 기록 ㄱㄱ</Text>
 			</TouchableOpacity>
