e5b433c091195ea36deecc535b6c880488c15083
gn-ioeo <thdrkdms1030@hanyang.ac.kr>
2025-11-30 21:36:57 +0900

    feat: 404에러시 공용 토스트 미표시

--- shared/config/queryClient.ts
+++ shared/config/queryClient.ts
@@ -1,8 +1,12 @@
 import { useToastStore } from "@/shared/store/toast";
 import { QueryCache, QueryClient } from "@tanstack/react-query";
+import { AxiosError } from "axios";
 
 const queryCache = new QueryCache({
-	onError: () => {
+	onError: (error: Error) => {
+		if (error instanceof AxiosError && error?.response?.status === 404) {
+			return;
+		}
 		useToastStore.getState().show("데이터를 불러오는데 실패했습니다.");
 	},
 });
